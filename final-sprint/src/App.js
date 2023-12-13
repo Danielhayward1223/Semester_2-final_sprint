57966df1c4007a3b9ccd04a7c87713d163976b60
Daniel <daniel.hayward@keyin.com>
2023-12-13 18:23:21 -0330

    Finished Nav bar. (I think)

--- final-sprint/src/App.js
+++ final-sprint/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Nav from "./components/nav";
 import Home from "./components/productlist";
+import Cart from "./components/cart.jsx";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Nav />} />
-          <Route path="/" element={<Nav />} />
+          <Route path="/cart" element={<Nav />} />
           <Route path="/" element={<Nav />} />
           <Route path="/" element={<Nav />} />
         </Routes>
