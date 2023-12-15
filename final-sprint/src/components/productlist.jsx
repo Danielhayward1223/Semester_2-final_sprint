4a2091a5904860878da4f57c24ebb416472423c1
Beck <rebecca.pond@keyin.com>
2023-12-14 22:52:45 -0330

    used the hook :3

--- final-sprint/src/components/productlist.jsx
+++ final-sprint/src/components/productlist.jsx
@@ -1,26 +1,12 @@
 import { useState, useEffect } from "react";
 import "../css/home.css";
 import Nav from "./nav";
+import useFetch from "./fetchData";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://fakestoreapi.com/products");
-        const data = await response.json();
-        setProducts(data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const fetchedData = useFetch();
+  const products = fetchedData[0];
+  const loading = fetchedData[1];
 
   return (
     <div>
