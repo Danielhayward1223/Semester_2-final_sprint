8d045d83f88a68b59f076403f600b1e116ba1505
JustMelloh <austinaustin2009@live.ca>
2023-12-17 19:28:22 -0330

    Worked on my portion.

--- final-sprint/src/App.js
+++ final-sprint/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Nav from "./components/nav";
 import Home from "./components/productlist";
 import Cart from "./components/cart.jsx";
+import Product  from "./components/product.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/" element={<Nav />} />
           <Route path="/" element={<Nav />} />
+          <Route path="/product/:productId" element={<Product />} />
         </Routes>
       </Router>
     </div>
